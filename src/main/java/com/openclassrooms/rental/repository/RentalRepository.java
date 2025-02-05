08a398ad566c9f338b3c5525692326bbe33b25a8
axelker <axelker@outlook.fr>
2025-02-05 17:48:32 +0100

    feat: support rentals table

---
+++ src/main/java/com/openclassrooms/rental/repository/RentalRepository.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.rental.repository;
+
+
+import com.openclassrooms.rental.model.RentalEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends JpaRepository<RentalEntity, Long> {}
