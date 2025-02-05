93adf3317a27babf88b2eeac67006f1e66adef30
axelker <axelker@outlook.fr>
2025-02-05 17:48:49 +0100

    feat: support users table

---
+++ src/main/java/com/openclassrooms/rental/model/UserEntity.java
@@ -0,0 +1,39 @@
+package com.openclassrooms.rental.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+    
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+
+}
