e2a2273a7fdc1153df9e09d10cd85faafac66c22
axelker <axelker@outlook.fr>
2025-02-06 21:57:53 +0100

    feat: impl global ex handler

---
+++ src/main/java/com/openclassrooms/rental/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.rental.exception;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException .class)
+    public ResponseEntity<String> handleUserNotFound(NoSuchElementException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
