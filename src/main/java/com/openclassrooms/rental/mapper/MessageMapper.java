bb1f424cc18bf0b2ed0ca104b84d430c571e2211
axelker <axelker@outlook.fr>
2025-02-10 15:44:08 +0100

    feat: add mapstruct to message mapper

--- src/main/java/com/openclassrooms/rental/mapper/MessageMapper.java
+++ src/main/java/com/openclassrooms/rental/mapper/MessageMapper.java
@@ -1,20 +1,33 @@
 package com.openclassrooms.rental.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
 import com.openclassrooms.rental.dto.request.MessageRequest;
 import com.openclassrooms.rental.model.MessageEntity;
 import com.openclassrooms.rental.model.RentalEntity;
 import com.openclassrooms.rental.model.UserEntity;
 
-public class MessageMapper {
-    
-    public static MessageEntity toEntity(MessageRequest message) {
-        if (message == null) {
-            return null;
-        }
-        MessageEntity entity = new MessageEntity();
-        entity.setUser(UserEntity.builder().id(message.getUser_id()).build());
-        entity.setRental(RentalEntity.builder().id(message.getRental_id()).build());
-        entity.setMessage(message.getMessage());
-        return entity;
+@Mapper
+public interface MessageMapper {
+
+    MessageMapper INSTANCE = Mappers.getMapper(MessageMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+
+    @Mapping(target = "user", source = "user_id", qualifiedByName = "mapUserById")
+    @Mapping(target = "rental", source = "rental_id", qualifiedByName = "mapRentalById")
+    MessageEntity toEntity(MessageRequest message);
+
+    @Named("mapUserById")
+    default UserEntity mapUserById(Integer userId) {
+        return userId == null ? null : UserEntity.builder().id(userId).build();
+    }
+
+    @Named("mapRentalById")
+    default RentalEntity mapRentalById(Integer rentalId) {
+        return rentalId == null ? null : RentalEntity.builder().id(rentalId).build();
     }
 }
---
+++ src/test/java/com/openclassrooms/rental/mapper/MessageMapperTest.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.rental.mapper;
+
+
+import org.junit.jupiter.api.Test;
+
+import com.openclassrooms.rental.dto.request.MessageRequest;
+import com.openclassrooms.rental.model.MessageEntity;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MessageMapperTest {
+    private final MessageMapper messageMapper = MessageMapper.INSTANCE;
+
+    @Test
+    void shouldMapMessageRequestNullToNull() {
+        MessageRequest message = null;
+        MessageEntity messageEntity = messageMapper.toEntity(message);
+
+        assertThat(messageEntity).isNull();
+    }
+
+    @Test
+    void shouldMapMapMessageRequestToMessageEntity() {
+        MessageRequest message = MessageRequest.builder().user_id(1).rental_id(2).message("test").build();
+        MessageEntity messageEntity = messageMapper.toEntity(message);
+
+        assertThat(messageEntity).isNotNull();
+        assertThat(messageEntity.getUser().getId()).isEqualTo(message.getUser_id());
+        assertThat(messageEntity.getRental().getId()).isEqualTo(message.getRental_id());
+        assertThat(messageEntity.getMessage()).isEqualTo(message.getMessage());
+    }
+}
