330e793f011805b4a2b2cded5ad0e17599409562
axelker <axelker@outlook.fr>
2025-02-10 15:56:19 +0100

    feat: use mapstruct for rental mapper

--- src/main/java/com/openclassrooms/rental/mapper/RentalMapper.java
+++ src/main/java/com/openclassrooms/rental/mapper/RentalMapper.java
@@ -1,27 +1,14 @@
 package com.openclassrooms.rental.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
 import com.openclassrooms.rental.dto.response.RentalResponse;
 import com.openclassrooms.rental.model.RentalEntity;
 
-public class RentalMapper {
+@Mapper
+public interface RentalMapper {
+    RentalMapper INSTANCE = Mappers.getMapper(RentalMapper.class);
 
-    public static RentalResponse toDto(RentalEntity entity) {
-        if (entity == null) {
-            return null;
-        }
-
-        RentalResponse response = RentalResponse
-                .builder()
-                .id(entity.getId())
-                .name(entity.getName())
-                .surface(entity.getSurface())
-                .price(entity.getPrice())
-                .picture(entity.getPicture())
-                .description(entity.getDescription())
-                .owner_id(entity.getOwner_id())
-                .created_at(entity.getCreated_at())
-                .updated_at(entity.getUpdated_at())
-                .build();
-        return response;
-    }
+    RentalResponse toDto(RentalEntity entity);
 }
