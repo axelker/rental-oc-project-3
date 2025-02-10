ba62efafa9617963323261735bfd4c95953d0b59
axelker <axelker@outlook.fr>
2025-02-10 15:44:20 +0100

    feat: use dto inside controller

--- src/main/java/com/openclassrooms/rental/controller/MessageRestController.java
+++ src/main/java/com/openclassrooms/rental/controller/MessageRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.rental.dto.request.MessageRequest;
 import com.openclassrooms.rental.dto.response.Response;
+import com.openclassrooms.rental.mapper.MessageMapper;
 import com.openclassrooms.rental.service.command.MessageCommandService;
 
 import org.springframework.http.ResponseEntity;
@@ -15,14 +16,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/messages")
 public class MessageRestController {
     private final MessageCommandService messageCommandService;
+    private final MessageMapper messageMapper;
 
-    public MessageRestController(MessageCommandService messageCommandService) {
+    public MessageRestController(MessageCommandService messageCommandService, MessageMapper messageMapper) {
         this.messageCommandService = messageCommandService;
+        this.messageMapper = messageMapper;
     }
 
     @PostMapping("")
     public ResponseEntity<Response> sendMessage(@RequestBody MessageRequest body) {
-        this.messageCommandService.createMessage(body);
+        this.messageCommandService.createMessage(messageMapper.toEntity(body));
         return ResponseEntity.ok(Response.builder().message("Message send with success !").build());
     }
 
--- src/main/java/com/openclassrooms/rental/service/command/MessageCommandService.java
+++ src/main/java/com/openclassrooms/rental/service/command/MessageCommandService.java
@@ -2,21 +2,19 @@ package com.openclassrooms.rental.service.command;
 
 import org.springframework.stereotype.Service;
 
-import com.openclassrooms.rental.dto.request.MessageRequest;
-import com.openclassrooms.rental.mapper.MessageMapper;
+import com.openclassrooms.rental.model.MessageEntity;
 import com.openclassrooms.rental.repository.MessageRepository;
 
 @Service
 public class MessageCommandService {
-    
+
     private final MessageRepository messageRepository;
 
-
-    public MessageCommandService(MessageRepository messageRepository){
+    public MessageCommandService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
 
-    public void createMessage(MessageRequest message) {
-        messageRepository.save(MessageMapper.toEntity(message));
+    public void createMessage(MessageEntity message) {
+        messageRepository.save(message);
     }
 }
