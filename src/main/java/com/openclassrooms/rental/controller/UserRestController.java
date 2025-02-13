acc7c42a740270fd6c479d201ef4d91fc04453ef
axelker <axelker@outlook.fr>
2025-02-13 11:55:49 +0100

    docs: impl swagger for user controller

--- src/main/java/com/openclassrooms/rental/controller/UserRestController.java
+++ src/main/java/com/openclassrooms/rental/controller/UserRestController.java
@@ -3,13 +3,27 @@ package com.openclassrooms.rental.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.rental.dto.response.ErrorResponse;
 import com.openclassrooms.rental.dto.response.UserResponse;
 import com.openclassrooms.rental.service.query.UserQueryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Tag(name = "Users", description = "Endpoints for managing user properties")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = "Unauthorized - Token missing or invalid", content = @Content),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class)))
+})
 @RestController
 @RequestMapping("/user")
 public class UserRestController {
@@ -20,6 +34,11 @@ public class UserRestController {
         this.userQueryService = userQueryService;
     }
 
+    @Operation(summary = "Get a user by ID", description = "Retrieve details of a user property using its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping("{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Integer id) {
         return ResponseEntity.ok(userQueryService.getUserById(id));
